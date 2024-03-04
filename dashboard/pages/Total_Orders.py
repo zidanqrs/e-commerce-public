9d170f36bd25fba5407b8b710c3f6e0b42246cff
zidanqrs <zidanqs@gmail.com>
2024-03-04 20:30:09 +0700

    Done

--- dashboard/pages/Total_Orders.py
+++ dashboard/pages/Total_Orders.py
@@ -57,6 +57,12 @@ orders_merge = pd.merge(orders, order_payments, how = 'inner',on = 'order_id')
 orders_date_cols = ['order_purchase_timestamp', 'order_approved_at', 'order_delivered_carrier_date', 'order_delivered_customer_date', 'order_estimated_delivery_date']
 orders_merge = change_to_datetime(orders_merge, orders_date_cols)
 orders_merge['Year'] = [x.year for x in orders_merge['order_purchase_timestamp']]
+orders_merge['Month'] = [x.month_name() for x in orders_merge['order_purchase_timestamp']]
+
+custom_month_order = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+]
 
 
 with st.sidebar:
@@ -74,10 +80,18 @@ if selected == '2016':
     order_time_of_day_series = pd.Series(order_time_of_day)
     day_orders_series = pd.Series(day_orders)
 
+    month_count_df = pd.DataFrame({
+    'Month' : orders_2016['Month'],
+    'Year' : orders_2016['Year']
+    })
+
+    month_count_df['Month'] = pd.Categorical(month_count_df['Month'], categories=custom_month_order, ordered=True)
+    sorted_month_count_df = month_count_df[month_count_df['Year'] == 2016].sort_values(['Year', 'Month'])
+    sorted_month_count_df = sorted_month_count_df.sort_values('Month')
+
     col1,col2=st.columns([10,10])
 
     with col1:
-        col1.success('First')
         fig1 = go.Figure()
 
         fig1 = px.histogram(day_orders_series,  
@@ -85,7 +99,7 @@ if selected == '2016':
         labels={'x': 'Order Status', 'y': 'Count'})
 
         fig1.update_layout(
-        title='Histogram of Order Status in 2016',
+        title='Total Orders by Day of Week in 2016',
         xaxis_title='Order Status',
         yaxis_title='Count',
         template='plotly_white',
@@ -95,7 +109,6 @@ if selected == '2016':
         col1.plotly_chart(fig1,use_container_width=True)
 
     with col2:
-        col2.success('Second')
         fig2 = go.Figure()
 
         fig2 = px.histogram(day_orders_series,  
@@ -103,7 +116,7 @@ if selected == '2016':
         labels={'x': 'Order Status', 'y': 'Count'})
 
         fig2.update_layout(
-        title='Histogram of Order Status in 2016',
+        title='Total Orders by Time of Day in 2016',
         xaxis_title='Order Status',
         yaxis_title='Count',
         template='plotly_white',
@@ -111,4 +124,254 @@ if selected == '2016':
     )
         
         col2.plotly_chart(fig2,use_container_width=True)
+
+    with st.container():
+        fig3 = go.Figure()
+
+        fig3 = px.histogram(sorted_month_count_df['Month'],  
+        category_orders={'x': sorted_month_count_df['Month'].value_counts().index},
+        labels={'x': 'Month', 'y': 'Count'})
         
+        fig3.update_layout(
+        title='Total Orders by Month of Years in 2016',
+        xaxis_title='Order Status',
+        yaxis_title='Count',
+        template='plotly_white',
+        width=1000,height=600
+    )
+        
+        st.plotly_chart(fig3,use_container_width=True)
+
+if selected == '2017':
+    orders_2017 = orders_merge[orders_merge['Year'] == 2017]
+
+    order_time_of_day = [get_time_of_day(x) for x in orders_2017['order_purchase_timestamp']]
+    day_orders = [x.day_name() for x in orders_2017['order_purchase_timestamp']]
+
+    order_time_of_day_series = pd.Series(order_time_of_day)
+    day_orders_series = pd.Series(day_orders)
+
+    month_count_df = pd.DataFrame({
+    'Month' : orders_2017['Month'],
+    'Year' : orders_2017['Year']
+    })
+
+    month_count_df['Month'] = pd.Categorical(month_count_df['Month'], categories=custom_month_order, ordered=True)
+    sorted_month_count_df = month_count_df[month_count_df['Year'] == 2017].sort_values(['Year', 'Month'])
+    sorted_month_count_df = sorted_month_count_df.sort_values('Month')
+
+    col1,col2=st.columns([10,10])
+
+    with col1:
+        fig1 = go.Figure()
+
+        fig1 = px.histogram(day_orders_series,  
+        category_orders={'x': day_orders_series.value_counts().index},
+        labels={'x': 'Order Status', 'y': 'Count'})
+
+        fig1.update_layout(
+        title='Total Orders by Day of Week in 2017',
+        xaxis_title='Order Status',
+        yaxis_title='Count',
+        template='plotly_white',
+        width=1000,height=600
+    )
+        
+        col1.plotly_chart(fig1,use_container_width=True)
+
+    with col2:
+        fig2 = go.Figure()
+
+        fig2 = px.histogram(day_orders_series,  
+        category_orders={'x': day_orders_series.value_counts().index},
+        labels={'x': 'Order Status', 'y': 'Count'})
+
+        fig2.update_layout(
+        title='Total Orders by Time of Day in 2017',
+        xaxis_title='Order Status',
+        yaxis_title='Count',
+        template='plotly_white',
+        width=1000,height=600
+    )
+        
+        col2.plotly_chart(fig2,use_container_width=True)
+
+    with st.container():
+        fig3 = go.Figure()
+
+        fig3 = px.histogram(sorted_month_count_df['Month'],  
+        category_orders={'x': sorted_month_count_df['Month'].value_counts().index},
+        labels={'x': 'Month', 'y': 'Count'})
+        
+        fig3.update_layout(
+        title='Total Orders by Month of Years in 2017',
+        xaxis_title='Order Status',
+        yaxis_title='Count',
+        template='plotly_white',
+        width=1000,height=600
+    )
+        
+        st.plotly_chart(fig3,use_container_width=True)
+
+if selected == '2018':
+    orders_2018 = orders_merge[orders_merge['Year'] == 2018]
+
+    order_time_of_day = [get_time_of_day(x) for x in orders_2018['order_purchase_timestamp']]
+    day_orders = [x.day_name() for x in orders_2018['order_purchase_timestamp']]
+
+    order_time_of_day_series = pd.Series(order_time_of_day)
+    day_orders_series = pd.Series(day_orders)
+
+    month_count_df = pd.DataFrame({
+    'Month' : orders_2018['Month'],
+    'Year' : orders_2018['Year']
+    })
+
+    month_count_df['Month'] = pd.Categorical(month_count_df['Month'], categories=custom_month_order, ordered=True)
+    sorted_month_count_df = month_count_df[month_count_df['Year'] == 2018].sort_values(['Year', 'Month'])
+    sorted_month_count_df = sorted_month_count_df.sort_values('Month')
+
+    col1,col2=st.columns([10,10])
+
+    with col1:
+        fig1 = go.Figure()
+
+        fig1 = px.histogram(day_orders_series,  
+        category_orders={'x': day_orders_series.value_counts().index},
+        labels={'x': 'Order Status', 'y': 'Count'})
+
+        fig1.update_layout(
+        title='Total Orders by Day of Week in 2018',
+        xaxis_title='Order Status',
+        yaxis_title='Count',
+        template='plotly_white',
+        width=1000,height=600
+    )
+        
+        col1.plotly_chart(fig1,use_container_width=True)
+
+    with col2:
+        fig2 = go.Figure()
+
+        fig2 = px.histogram(day_orders_series,  
+        category_orders={'x': day_orders_series.value_counts().index},
+        labels={'x': 'Order Status', 'y': 'Count'})
+
+        fig2.update_layout(
+        title='Total Orders by Time of Day in 2018',
+        xaxis_title='Order Status',
+        yaxis_title='Count',
+        template='plotly_white',
+        width=1000,height=600
+    )
+        
+        col2.plotly_chart(fig2,use_container_width=True)
+
+    with st.container():
+        fig3 = go.Figure()
+
+        fig3 = px.histogram(sorted_month_count_df['Month'],  
+        category_orders={'x': sorted_month_count_df['Month'].value_counts().index},
+        labels={'x': 'Month', 'y': 'Count'})
+        
+        fig3.update_layout(
+        title='Total Orders by Month of Years in 2018',
+        xaxis_title='Order Status',
+        yaxis_title='Count',
+        template='plotly_white',
+        width=1000,height=600
+    )
+        
+        st.plotly_chart(fig3,use_container_width=True)
+
+if selected == 'All':
+    orders_all = orders_merge
+
+    order_time_of_day = [get_time_of_day(x) for x in orders_all['order_purchase_timestamp']]
+    day_orders = [x.day_name() for x in orders_all['order_purchase_timestamp']]
+
+    order_time_of_day_series = pd.Series(order_time_of_day)
+    day_orders_series = pd.Series(day_orders)
+
+    month_count_df = pd.DataFrame({
+    'Month' : orders_all['Month'],
+    'Year' : orders_all['Year']
+    })
+
+    month_count_df['Month'] = pd.Categorical(month_count_df['Month'], categories=custom_month_order, ordered=True)
+    sorted_month_count_df = month_count_df.sort_values(['Year', 'Month'])
+    sorted_month_count_df = sorted_month_count_df.sort_values('Month')
+
+    col1,col2=st.columns([10,10])
+
+    with col1:
+        fig1 = go.Figure()
+
+        fig1 = px.histogram(day_orders_series,  
+        category_orders={'x': day_orders_series.value_counts().index},
+        labels={'x': 'Order Status', 'y': 'Count'})
+
+        fig1.update_layout(
+        title='Total Orders by Day of Week in 2016-2018',
+        xaxis_title='Order Status',
+        yaxis_title='Count',
+        template='plotly_white',
+        width=1000,height=600
+    )
+        
+        col1.plotly_chart(fig1,use_container_width=True)
+
+    with col2:
+        fig2 = go.Figure()
+
+        fig2 = px.histogram(day_orders_series,  
+        category_orders={'x': day_orders_series.value_counts().index},
+        labels={'x': 'Order Status', 'y': 'Count'})
+
+        fig2.update_layout(
+        title='Total Orders by Time of Day in 2016-2018',
+        xaxis_title='Order Status',
+        yaxis_title='Count',
+        template='plotly_white',
+        width=1000,height=600
+    )
+        
+        col2.plotly_chart(fig2,use_container_width=True)
+
+    with st.container():
+        fig3 = go.Figure()
+
+        #fig3 = px.histogram(sorted_month_count_df['Month'],  
+        #category_orders={'x': sorted_month_count_df['Month'].value_counts().index},
+        #labels={'x': 'Month', 'y': 'Count'})
+        
+        fig3.add_trace(go.Histogram(
+            x=sorted_month_count_df.loc[sorted_month_count_df['Year']==2016,'Month'],
+            name='2016', # name used in legend and hover labels
+            marker_color='#50C4ED',
+            opacity=0.75
+        ))
+
+        fig3.add_trace(go.Histogram(
+            x=sorted_month_count_df.loc[sorted_month_count_df['Year']==2017,'Month'],
+            name='2017', # name used in legend and hover labels
+            marker_color='#387ADF',
+            opacity=0.75
+        ))
+
+        fig3.add_trace(go.Histogram(
+            x=sorted_month_count_df.loc[sorted_month_count_df['Year']==2018,'Month'],
+            name='2018', # name used in legend and hover labels
+            marker_color='#333A73',
+            opacity=0.75
+        ))
+
+        fig3.update_layout(
+        title='Total Orders by Month of Years in 2016-2018',
+        xaxis_title='Order Status',
+        yaxis_title='Count',
+        template='plotly_white',
+        width=1000,height=600
+    )
+        
+        st.plotly_chart(fig3,use_container_width=True)
